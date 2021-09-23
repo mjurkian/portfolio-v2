c9940a263701ce8f048207e637348938aa3f1d0c
Michal Jurkian <michal@kukidigital.com>
2021-09-22 22:16:40 -0600

    Update hero image

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,18 +2,25 @@
   <section id="hero" class="flex relative items-center justify-center max-w-screen-lg min-h-screen mx-auto">
     <div class="container flex flex-col-reverse lg:flex-row items-center gap-12 md:mx-auto">
       <div class="flex justify-center flex-1 lg:order-1 mb-10 md:mb-16 lg:mb-0 z-10">
-        <img alt="" class="w-5/6 h-5/6 sm:3/4 rounded-3xl shadow-2xl" src="../assets/cat-min.jpg">
+        <img alt="" class="w-5/6 h-5/6 sm:3/4 rounded-3xl shadow-2xl opacity-80" src="../assets/mj.png">
       </div>
       <div class="flex flex-1 flex-col items-center lg:items-start">
         <h1 class="mb-8 text-4xl md:text-7xl text-mj-php-mellow">Hi, I'm Michal</h1>
         <p class="text-mj-lavender text-2xl">a Husband, a Father, a Web Developer.</p>
       </div>
     </div>
+
+<!--    <ArrowDownIcon class="absolute left-1/2 bottom-4 animate-bounce text-mj-php-mellow  w-8 h-8 " />-->
   </section>
 </template>
 
 <script>
+// import { ArrowDownIcon } from '@heroicons/vue/outline'
+
 export default {
   name: 'Hero',
+  components: {
+    // ArrowDownIcon
+  }
 }
 </script>
